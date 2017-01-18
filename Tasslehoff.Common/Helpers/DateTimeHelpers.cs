e4430ed22cde9e5f407b27ce616aec9bf9601c10
Eser Ozvataf <eser@ozvataf.com>
2017-01-18 14:10:35 +0300

    Added Tassle.Tasks.

--- Tasslehoff.Common/Helpers/DateTimeHelpers.cs
+++ Tasslehoff.Common/Helpers/DateTimeHelpers.cs
@@ -1,7 +1,7 @@
 // --------------------------------------------------------------------------
 // <copyright file="DateTimeHelpers.cs" company="-">
 // Copyright (c) 2008-2017 Eser Ozvataf (eser@ozvataf.com). All rights reserved.
-// Web: http://eser.ozvataf.com/ GitHub: http://github.com/larukedi
+// Web: http://eser.ozvataf.com/ GitHub: http://github.com/eserozvataf
 // </copyright>
 // <author>Eser Ozvataf (eser@ozvataf.com)</author>
 // --------------------------------------------------------------------------
